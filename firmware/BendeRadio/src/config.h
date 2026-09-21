ba2e55db3b7f1518ef659aeb20386a8e7852a650
v.martsun <v.martsun@spd.tech>
2026-09-21 12:26:05 +0300

    Implement bluetooth connection. Remove radio

--- firmware/BendeRadio/src/config.h
+++ firmware/BendeRadio/src/config.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#define AP_SSID "***"
-#define AP_PASS "***"
-
 #define I2S_DOUT 25
 #define I2S_BCLK 27
 #define I2S_LRC 26
@@ -18,4 +15,3 @@
 #define ANALYZ_PIN 34
 
 #define ANALYZ_WIDTH (3 * 8)
-#define RADIO_BUFFER (1600 * 25)  // default 1600*5, этого МАЛО
