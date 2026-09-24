4f6268c8c3c1c4cb38babd1378ee9e6641c605b9
v.martsun <v.martsun@spd.tech>
2026-09-24 22:32:10 +0300

    Remove redundant imports from main

--- firmware/BendeRadio/src/main.cpp
+++ firmware/BendeRadio/src/main.cpp
@@ -1,24 +1,19 @@
 #include <Arduino.h>
-#include <WiFi.h>
 
+#include "btAudio.h"
 #include "config.h"
 #include "core0.h"
-#include "btAudio.h"
-#include <esp_a2dp_api.h>
 
 TaskHandle_t Task0;
 btAudio btaudio = btAudio("Bender BT");
 
-
-
 void setup() {
-    xTaskCreatePinnedToCore(core0, "Task0", 10000, NULL, 1, &Task0, 0);
     Serial.begin(115200);
-   
-    change_state();
+    delay(500);
+    Serial.println(F("\n=== BOOT ==="));
+    xTaskCreatePinnedToCore(core0, "Task0", 20480, NULL, 1, &Task0, 0);
 }
 
 void loop() {
     vTaskDelay(pdMS_TO_TICKS(100));
 }
-
